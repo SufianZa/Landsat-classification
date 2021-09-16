29c983e111f372cb24d07f10d7b058901fd74b68
EHJ-52n <e.h.juerrens@52north.org>
2021-09-16 10:05:56 +0200

    Fix imports of keras
    Prior imports resulted in mixed keras versions
    
    See
    https://stackoverflow.com/questions/50056356/could-not-interpret-optimizer-identifier-error-in-keras/50066670#50066670
    
    Co-Authored-By: Sufian Zaabalawi <m.s.zaabalawi@52north.org>

--- src/landsatpredictor/u_net.py
+++ src/landsatpredictor/u_net.py
@@ -20,12 +20,12 @@ import numpy as np
 import rasterio
 from PIL import Image
 from matplotlib import pyplot as plt
-from tensorflow.keras import backend as K
-from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D
-from tensorflow.keras.models import Model, load_model
+from tensorflow.python.keras import backend as K
 from tensorflow.python.keras.backend import concatenate
 from tensorflow.python.keras.callbacks import ModelCheckpoint, EarlyStopping
 from tensorflow.python.keras.layers import Conv2DTranspose, Dropout
+from tensorflow.python.keras.layers import Input, Conv2D, MaxPooling2D
+from tensorflow.python.keras.models import Model, load_model
 from tensorflow.python.keras.optimizer_v2.adam import Adam
 from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
 
